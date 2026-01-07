2d1dd3c584d13751aa7fbeedc5794bef82891725
PriyankaBadgujar998 <priyankabadgujar998@gmail.com>
2026-01-07 16:36:39 +0530

    Add files via upload

---
+++ count1.py
@@ -0,0 +1,6 @@
+#Write a while loop to count from 1 to 10.
+counter = 1
+while counter<=10:
+    print(counter)
+    counter +=1
+
---
+++ count10.py
@@ -0,0 +1,6 @@
+#Write a while loop to print the numbers in a countdown from 10 to 1.
+counter = 10
+while counter>=1:
+    print(counter)
+    counter -=1
+    
