e6fb3e10db506b83b49cb554a5350f4918bd0a47
PriyankaBadgujar998 <priyankabadgujar998@gmail.com>
2026-01-12 18:55:54 +0530

    Add files via upload

---
+++ countertable5.py
@@ -0,0 +1,13 @@
+# number = int(input("Enter the number:"))
+# counter = number
+# while counter <= number * 10:
+#   print(counter)
+#   counter +=number
+
+
+
+number = int(input("Enter the number:"))
+counter = number * 10
+while counter >= number :
+  print(counter)
+  counter -= number
